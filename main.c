69d2b406db83f873099fdfd235f57757585086c4
jeremiah <jeremiahbl@protonmail.com>
2026-01-04 23:55:30 -0600

    main.c: code cleanup, minor bugfixes, support adding entires

--- main.c
+++ main.c
@@ -30,28 +30,31 @@ static const cfg_type_t types[] = {
 	_CFG_ENTRY(STRING, string, s, const char*)
 //	{ 0, "auto", "a", 0, NULL, NULL }, TODO
 	{ 0, NULL, NULL, 0, NULL, NULL },
-	/*{ CONFIG_TYPE_INT,    "int",    "i", (config_lookup_func_t) config_lookup_int,    sizeof(int) },
-	{ CONFIG_TYPE_INT64,  "int64",  "l", (config_lookup_func_t) config_lookup_int64,  sizeof(long long) },
-	{ CONFIG_TYPE_FLOAT,  "float",  "f", (config_lookup_func_t) config_lookup_float,  sizeof(double) },
-	{ CONFIG_TYPE_BOOL,   "bool",   "b", (config_lookup_func_t) config_lookup_bool,   sizeof(int) },
-	{ CONFIG_TYPE_STRING, "string", "s", (config_lookup_func_t) config_lookup_string, sizeof(const char*) },*/
 };
 
 int usage(char *prog, char *msg);
 int get_type(char *tstr, cfg_type_t *type);
 int print_cfg_err(const config_t *cfg, const char *func);
+char* strip_dashes(char*);
 
-int read_cfg(const config_t *cfg, char *tstr, char *key) {
+int read_cfg(const config_t *cfg, char *tstr, char *key, char *val) {
 	cfg_type_t type;
 	volatile void *res;
 	int ret;
 
-	// TODO support no type
+	// TODO handle default values
+	if(val)
+		return ENOTSUP;
+
+	// TODO support automatic type detection via config_setting_type
 	ret = get_type(tstr, &type);
 	if(ret)
 		return ret;
 
+	/* allocate the value */
 	res = alloca(type.size);
+
+	/* read the value */
 	ret = type.lookup(cfg, key, res);
 
 	if(ret == CONFIG_TRUE) {
@@ -73,48 +76,56 @@ int write_cfg(const config_t *cfg, char *tstr, char *key, char *val) {
 	cfg_type_t type;
 	int ret;
 
+	// TODO support automatic type detection via config_setting_type
 	ret = get_type(tstr, &type);
 	if(ret)
 		return ret;
 
+	/* get the conffiguration setting */
 	sett = config_lookup(cfg, ((const char*) key));
-	if(sett) {
-		res = alloca(type.size);
-		errno = 0;
-		switch(type.id) {
-			case CONFIG_TYPE_INT:    ret = sscanf(val, "%d\n", (int*) res); break;
-			case CONFIG_TYPE_INT64:  ret = sscanf(val, "%lld\n", (long long*) res); break;
-			case CONFIG_TYPE_FLOAT:  ret = sscanf(val, "%lf\n", (double*) res); break;
-			case CONFIG_TYPE_STRING: ret = strlen(val); res = (volatile void*) &val; break;
-			case CONFIG_TYPE_BOOL:
-				if(strcasecmp(val, "true") == 0    || strcasecmp(val, "yes") == 0 || strcasecmp(val, "y") == 0 || strcasecmp(val, "t") == 0) {
-					ret = strlen(val); *((int*) res) = 1;
-				} else if(strcasecmp(val, "false") == 0 || strcasecmp(val, "no") == 0  || strcasecmp(val, "n") == 0 || strcasecmp(val, "f") == 0) {
-					ret = strlen(val); *((int*) res) = 0;
-				} else {
-					ret = sscanf(val, "%d\n", (int*) res);
-				}
-				break;
-		}
-		if(errno || ret == 0 || ret == EOF) {
-			fprintf(stderr, "Error: sscanf failed to parse value!\n");
-			if(errno) fprintf(stderr, "ERRNO %d - %s\n", errno, strerror(errno));
-			else if(ret == EOF) fprintf(stderr, "Reached EOF before a conversion!\n");
-			else fprintf(stderr, "Was unable to match any bytes!\n");
-			return errno ? errno : -1;
-		}
-
-		ret = type.setting_set(sett, res);
-		if(ret == CONFIG_TRUE) {
-			return 0;
-		} else {
-			return print_cfg_err(cfg, "config_setting_set");
-		}
-	} else {
-		return print_cfg_err(cfg, "config_lookup");
+	if(sett == NULL) {
+		sett = config_root_setting(cfg);
+		if(sett == NULL)
+			return print_cfg_err(cfg, "config_root_setting");
+		sett = config_setting_add(sett, (const char*) key, type.id);
+		if(sett == NULL)
+			return print_cfg_err(cfg, "config_setting_add");
 	}
 
-	return ret;
+	/* parse the value */
+	res = alloca(type.size);
+	errno = 0;
+	switch(type.id) {
+		case CONFIG_TYPE_INT:    ret = sscanf(val, "%d\n", (int*) res); break;
+		case CONFIG_TYPE_INT64:  ret = sscanf(val, "%lld\n", (long long*) res); break;
+		case CONFIG_TYPE_FLOAT:  ret = sscanf(val, "%lf\n", (double*) res); break;
+		case CONFIG_TYPE_STRING: ret = strlen(val); res = (volatile void*) &val; break;
+		case CONFIG_TYPE_BOOL:
+			if(strcasecmp(val, "true") == 0    || strcasecmp(val, "yes") == 0 || strcasecmp(val, "y") == 0 || strcasecmp(val, "t") == 0) {
+				ret = strlen(val); *((int*) res) = 1;
+			} else if(strcasecmp(val, "false") == 0 || strcasecmp(val, "no") == 0  || strcasecmp(val, "n") == 0 || strcasecmp(val, "f") == 0) {
+				ret = strlen(val); *((int*) res) = 0;
+			} else {
+				ret = sscanf(val, "%d\n", (int*) res);
+			}
+			break;
+	}
+
+	/* ensure scanf actually worked */
+	if(errno || ret == 0 || ret == EOF) {
+		fprintf(stderr, "Error: sscanf failed to parse value!\n");
+		if(errno) fprintf(stderr, "ERRNO %d - %s\n", errno, strerror(errno));
+		else if(ret == EOF) fprintf(stderr, "Reached EOF before a conversion!\n");
+		else fprintf(stderr, "Was unable to match any bytes!\n");
+		return errno ? errno : -1;
+	}
+
+	/* store the value */
+	ret = type.setting_set(sett, res);
+	if(ret != CONFIG_TRUE)
+		return print_cfg_err(cfg, "config_setting_set");
+
+	return 0;
 }
 int dump_cfg(config_t *cfg) {
 	// TODO
@@ -124,24 +135,20 @@ int dump_cfg(config_t *cfg) {
 int main(int argc, char** argv) {
 	int ret, cfg_ret, cfg_write;
 	config_t cfg;
-	char *mode, *fname, *type, *key, *val;
-	mode = fname = type = val = key = NULL;
+	char *mode, *fname;
+	char *type, *key, *val;
 
 	/* check for -h/--help */
 	for(int i = 0; i < argc; i++)
 		if(strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0)
 			return usage(argv[0], NULL);
 
-	/* make sure there is at least a <filename> and <mode> argument */
-	if(argc < 3)
-		return usage(argv[0], "Not enough arguments!");
-
 	/* handle the arguments */
-	fname = argv[1];
-	mode = argv[2];
+	fname = argc > 1 ? argv[1] : NULL;
+	mode  = argc > 2 ? argv[2] : NULL;
 	if(fname && mode) {
-		/* remove any leading dashes from mdoe */
-		while(*mode == '-') mode++;
+		/* remove any leading dashes from mode */
+		mode = strip_dashes(mode);
 
 		/* check if mode is help */
 		if(strcmp(mode, "help") == 0 || strcmp(mode, "h") == 0)
@@ -160,9 +167,12 @@ int main(int argc, char** argv) {
 			}
 
 			/* grab excess arguments */
-			if(argc > 3) type = argv[3];
-			if(argc > 4) key = argv[4];
-			if(argc > 5) val = argv[5];
+			type = argc > 3 ? argv[3] : NULL;
+			key  = argc > 4 ? argv[4] : NULL;
+			val  = argc > 5 ? argv[5] : NULL;
+
+			/* remove any leading dashes from type */
+			type = strip_dashes(type);
 
 			/* check for dump mode first, its the easiest */
 			if(strcmp(mode, "dump") == 0 || strcmp(mode, "d") == 0) {
@@ -172,7 +182,7 @@ int main(int argc, char** argv) {
 				if(!key)  { ret = usage(argv[0], "Key wasn't given for write command!");   if(ret) goto exit_with_ret; }
 				if(ret)
 					goto exit_with_ret;
-				ret = read_cfg(&cfg, type, key);
+				ret = read_cfg(&cfg, type, key, val);
 			} else if(strcmp(mode, "write") == 0 || strcmp(mode, "w") == 0) {
 				if(!type) { ret = usage(argv[0], "Type wasn't given for write command!");  if(ret) goto exit_with_ret; }
 				if(!key)  { ret = usage(argv[0], "Key wasn't given for write command!");   if(ret) goto exit_with_ret; }
@@ -186,6 +196,10 @@ int main(int argc, char** argv) {
 				ret = EINVAL;
 			}
 
+			if(ret)
+				goto exit_with_ret;
+
+			/* update the config file if needed */
 			if(cfg_write) {
 				cfg_ret = config_write_file(&cfg, fname);
 				if(cfg_ret != CONFIG_TRUE) {
@@ -200,11 +214,13 @@ int main(int argc, char** argv) {
 			fprintf(stderr, "Error: File \"%s\" does not exist!\n", fname);
 			return EINVAL;
 		}
+	} else {
+		return usage(argv[0], "Not enough arguments!");
 	}
-	fprintf(stderr, "Error: Filename / Mode is nullptr, cannot continue!\n");
-	return -1;
 
 exit_with_ret:
+	if(ret == ENOTSUP)
+		fprintf(stderr, "Error: A feature used is not yet supported!\n");
 	config_destroy(&cfg);
 	return ret;
 }
@@ -226,7 +242,7 @@ int get_type(char *tstr, cfg_type_t *type) {
 		}
 	}
 
-	fprintf(stderr, "Error: Invalid / Unknown type \"%s\" given!", tstr);
+	fprintf(stderr, "Error: Invalid / Unknown type \"%s\" given!\n", tstr);
 	return EINVAL;
 }
 int usage(char *prog, char *msg) {
@@ -256,7 +272,7 @@ int usage(char *prog, char *msg) {
 		"Modes:\n"
 //		"  d/dump  - dumps all configuration values\n"
 		"  r/read  - reads configuratiuon values\n"
-		"            expects: <type> <key>\n"
+		"            expects: <type> <key> [default]\n"
 		"  w/write - sets configuration value\n"
 		"            expects: <type> <key> <val>\n"
 		"Accepted types:\n  %s\n"
@@ -287,4 +303,9 @@ int print_cfg_err(const config_t *cfg, const char *func) {
 			fprintf(stderr, "Unknown error?!\n");
 			return -1;
 	}
+}
+char* strip_dashes(char* str) {
+	while(str && *str == '-')
+		str++;
+	return str;
 }
